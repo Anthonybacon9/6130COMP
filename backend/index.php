2b32d72a4d262a91e8ab9b6b50d4cebc0bb684a9
anthonybacon916@gmail.com <anthonybacon916@gmail.com>
2023-03-31 04:29:50 +0100

    compose

--- backend/index.php
+++ backend/index.php
@@ -2,8 +2,8 @@
     $data = json_decode(file_get_contents('php://input'), true);
     $name = $data['name'];
     $email = $data['email'];
-    $phone = $data['phone'];
-    $message = $data['message'];
+    $Address = $data['Address'];
+    $player = $data['player'];
     $file = fopen("data.csv", "a");
     fputcsv($file, [$name, $email, $phone, $message]);
     fclose($file);
